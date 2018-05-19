0165fe5ecf532982af686c75c9a1af4ae7ab565d
jeans <jeans@DESKTOP-3N4U4AM>
2018-05-19 00:56:35 -0300

    Nova rede.

--- PerceptronBigData/src/BigDataPerceptron.java
+++ PerceptronBigData/src/BigDataPerceptron.java
@@ -21,23 +21,13 @@ public class BigDataPerceptron {
 	public static void main(String[] args) {
 
 		// TODO Auto-generated method stub
-
-		Scanner reader = new Scanner(System.in);
-
 		List<Integer> neuronsInLayers = new ArrayList<Integer>();
-
 		System.out.println("Criar a rede neural:");
-
 		System.out.println("<==============================================>");
-
 		CreatePerceptron.createNetworkPerceptron();
-
 		System.out.println("<==============================================>");
-
 		System.out.println("Testar a rede neural:");
-
 		LoadPerceptron.generateNeural();
-
 		System.out.println("<==============================================>");
 
 	}
